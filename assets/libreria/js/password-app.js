07fbbe9880026b58aed1813423260d2ac0abb038
helenharnisch <harnischvioleta@gmail.com>
2018-01-29 17:36:36 -0300

    Agregando carpetas y modificando la funcion nombre para llamarla desde el DOM

---
+++ assets/libreria/js/password-app.js
@@ -0,0 +1,20 @@
+const inputPassword = document.getElementById('sendbtn');
+inputPassword.addEventListener('click', function() {
+  let password = document.getElementById('contraseña').value;
+  let texto;
+  let expresion = /[0-9]/;
+
+  if (password === null || password === '' || password.length === 0) {
+    texto = '<span style= "color:red;">Ingrese su contraseña</span>';
+    document.getElementById('texto2').innerHTML = texto;
+    return false;
+  } else if (password.length < 3) {
+    texto = 'Su contraseña tiene que ser mayor o igual a 3 caracteres';
+    document.getElementById('texto2').innerHTML = texto;
+    return false;
+  } else if (!expresion.test(password)) {
+    texto = 'Ingrese solo caracteres válidos';
+    document.getElementById('texto2').innerHTML = texto;
+    return false;
+  }
+});
