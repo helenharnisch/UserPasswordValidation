4be4fdba2e54a3b11e2aaf0a2db35a9cd8832e8a
helenharnisch <harnischvioleta@gmail.com>
2018-02-01 18:46:36 -0300

    Subiendo demo

--- assets/libreria/js/password-app.js
+++ assets/libreria/js/password-app.js
@@ -1,28 +1,28 @@
 'use strict';
-const validarPassword = () => {
-  let password = document.getElementById('contraseña').value;
+const validatePassword = () => {
+  let password = document.getElementById('password').value;
   let texto;
   let expresion = /[0-9]/;
 
   if (password === null || password === '' || password.length === 0) {
-    texto = 'Ingrese su contraseña';
+    texto = '<span style="color: #fff; background-color: #e6213f; padding: 3px;border-radius: 3px;">Ingrese su nombre</span>';
     document.getElementById('texto2').innerHTML = texto;
     return false;
   } else if (password.length < 3) {
-    texto = 'Su contraseña tiene que ser mayor a 3 caracteres ';
+    texto = '<span style="color: #fff; background-color: #e6213f; padding: 3px;border-radius: 3px;">Tiene que ser mayor a 3 caracteres</span>';
     document.getElementById('texto2').innerHTML = texto;
     return false;
-  } else if (password.length > 7 ) {
-    texto = 'Su contraseña tiene que ser menor a 7 caracteres ';
+  } else if (password.length > 7) {
+    texto = '<span style="color: #fff; background-color: #e6213f; padding: 3px;border-radius: 3px;">Su contraseña tiene que ser menor a 7 caracteres</span>';
     document.getElementById('texto2').innerHTML = texto;
     return false;
   } else if (!expresion.test(password)) {
-    texto = 'Ingrese solo caracteres válidos (números)';
+    texto = '<span style="color: #fff; background-color: #e6213f; padding: 3px;border-radius: 3px;">Ingrese solo caracteres válidos (números)</span>';
     document.getElementById('texto2').innerHTML = texto;
     return false;
-  } 
-  document.getElementById('contraseña').value = '';
+  }
+  document.getElementById('password').value = '';
   document.getElementById('texto2').innerHTML = '';
 };
 
-module.exports = validarPassword;
+module.exports = validatePassword;
